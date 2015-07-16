9e77536872823f29eb03917408aa5935a4dc0666
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-07-15 22:48:51 -0700

    add src/mac/main.cpp

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+set -o nounset
+set -o errexit
+set -o pipefail
+
 # temporary script for building parts of code on mac
 # there are a lot of changes needed so we go bit-by-bit,
 # starting with the simplest, lowest-level code
 
-mkdir -p obj-mac
-clang++ -std=c++11 -g src/utils/BaseUtil.cpp -o obj-mac/BaseUtil.o
- 
+function build() {
+	DST_DIR=$1
+	rm -rf $DST_DIR
+	mkdir -p $DST_DIR
+	clang++ -std=c++11 -g -c src/utils/BaseUtil.cpp -Isrc/utils -o $DST_DIR/BaseUtil.o
+	clang++ -std=c++11 -g -c src/mac/main.cpp -Isrc/utils -o $DST_DIR/mac.o
+	clang++ -g $DST_DIR/*.o -o $DST_DIR/mac_main
+	./$DST_DIR/mac_main
+	ls -lah ./$DST_DIR
+	rm -rf $DST_DIR
+}
+
+build rel-mac
+
+# TODO: also need to change compilation flags, like DEBUG=1
+#build dbg-mac
---
+++ src/mac/main.cpp
@@ -0,0 +1,5 @@
+#include "BaseUtil.h"
+
+int main(int argc, char **argv) {
+	printf("Hello from mac\n");
+}
